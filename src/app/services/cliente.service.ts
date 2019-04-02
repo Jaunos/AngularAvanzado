433f176dc0de529f701614d27d8da70d5e8a6f03
Juan J. Lopez <juancorvelle@gmail.com>
2019-04-02 23:10:48 +0200

    Carpeta 10

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -1,18 +1,20 @@
+import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import {CLIENTES } from '../clientes/clientes.json';
 import { Cliente } from '../clientes/cliente';
-import { Observable, of } from 'rxjs';
+import { Observable, of , throwError} from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
+import swal from 'sweetalert2';
 
 
- 
+
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
   //Se define el endpoint http
-  private urlEndPoint: string = 'http://localhost:8080/api/clientes'; 
+  private urlEndPoint: string = 'http://localhost:3621/api/clientes';
 
   //Atributo para las cabeceras http
   private httpHeaders = new HttpHeaders({
@@ -20,7 +22,8 @@ export class ClienteService {
   })
 
   //Se importa http para la comunicación con el servidor
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private router: Router) { }
 
   // getClientes(): Cliente[] {
   //   return CLIENTES;
@@ -45,23 +48,61 @@ export class ClienteService {
   //metodo para crear clientes, recibe un cliente en formato JSON y devuelve un observable. Post crea datos en el
   //servidor rest.
   create(cliente: Cliente): Observable<Cliente> {
-    return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.httpHeaders})
+    return this.http.post(this.urlEndPoint, cliente, {headers: this.httpHeaders})
+    .pipe(map((response: any) => response.cliente as Cliente),
+      catchError(e => {
+        console.error(e.error.mensaje);
+        swal('Error al crear el cliente ', e.error.mensaje, 'error');
+        //Se devuelve el error en un tipo Observable
+        return throwError(e);
+      })
+    );
   }
 
 
   //método para obtener el cliente que se desea modificar
   getCliente(id) : Observable<Cliente>  {
-    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`)
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
+      //Recibe un 404 cuando no se encuentra el objeto
+      catchError(e => {
+        if (e.status == 400){
+           return throwError(e);
+        }
+        //Una vez capturado el error se redirige al cliente
+        this.router.navigate(['/clientes']);
+        console.log(e.error.mensaje);
+        swal('Error al editar ', e.error.mensaje, 'error');
+        //Se devuelve el error en un tipo Observable
+        return throwError(e);
+      })
+    )
   }
-  
+
 
   //metodo para actualizar clientes. Put actualiza datos en el servidor rest.
-  update(cliente: Cliente): Observable<Cliente> {
-    return this.http.put<Cliente>(`${this.urlEndPoint}/${cliente.id}`, cliente,  {headers: this.httpHeaders} )
+  update(cliente: Cliente): Observable<any> {
+    return this.http.put<any>(`${this.urlEndPoint}/${cliente.id}`, cliente,  {headers: this.httpHeaders} )
+     .pipe(catchError(e => {
+        if (e.status == 400){
+           return throwError(e);
+        }
+        console.error(e.error.mensaje);
+        swal('Error al editar el cliente ', e.error.mensaje, 'error');
+        //Se devuelve el error en un tipo Observable
+        return throwError(e);
+      })
+    );
   }
 
   //metodo para eliminar clientes. delete elimina datos en el servidor rest.
   delete(id: number): Observable<Cliente> {
     return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`, {headers: this.httpHeaders})
+     .pipe(catchError(e => {
+        console.error(e.error.mensaje);
+        swal('Error al eliminar el cliente ', e.error.mensaje, 'error');
+        //Se devuelve el error en un tipo Observable
+        return throwError(e);
+      })
+    );
   }
 }
